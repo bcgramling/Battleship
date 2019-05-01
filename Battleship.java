224f032ab5d645a6c3f8f5db898b38f47ab20a26
Bryce Gramling <Brycegramling@uiowq.edu>
2019-04-30 19:44:01 -0500

    V2 completed. Fixed bugs

--- Battleship.java
+++ Battleship.java
@@ -2,48 +2,44 @@ import java.util.Scanner;
 
 public class Battleship {
 	
-	public static void main(String[] args){
-		int playerScore=0;
+	public static void main(String[] args){				//Main Function: Starts program and calls each function to set up board and play game
+		int playerScore=0;															
 		int cpuScore=0;
 		int turn=0;
 		
 		System.out.println(" ");
 		System.out.println("***********BATTLESHIP***********");
 		System.out.println();
-		System.out.println("Board for displaying players boats");
+		System.out.println("Players boats");
 		
-		String [][] playerBoard = new String[11][11];							//creates a new double array for the player's board
-		setup.createBoard(playerBoard);
-		
-		
-		setup.drawBoard(playerBoard);
-		playerBoard= setup.setPiece(playerBoard);									//Calls setPiece function to have player set up their boats			
-		//setup.cpuSetup(playerBoard);												//shortcut for randomized player start. Commented out for normal play
-		setup.drawBoard(playerBoard);
+		String [][] playerBoard = new String[11][11];		//creates a new double array for the player's board
+		setup.createBoard(playerBoard);						//calls create board function to create the player's board
+		setup.drawBoard(playerBoard);						//Draws the board for the player		
+		playerBoard= setup.setPiece(playerBoard);			//Calls setPiece function to have player set up their boats			
+		//setup.cpuSetup(playerBoard);						//shortcut for randomized player start. Commented out for normal play
+		setup.drawBoard(playerBoard);						//draws the player's board
 		
 		System.out.println();
-		System.out.println("Board for displaying player's guesses");
-		String [][] playerGuessBoard = new String[11][11];	
-		setup.createBoard(playerGuessBoard);
-		setup.drawBoard(playerGuessBoard);
-		
-		
-		System.out.println();
-		//System.out.println("Board for displaying Cpu's boats");
-		String [][] cpuBoard = new String[11][11];										//creates a new double array for the cpu's board
-		setup.createBoard(cpuBoard);
-		setup.cpuSetup(cpuBoard);
-		//setup.drawBoard(cpuBoard);													//Draws the cpu's board. Commented out for regular play
+		System.out.println("Player's guesses");			
+		String [][] playerGuessBoard = new String[11][11];	//Creates a new double array for the player's guesses	
+		setup.createBoard(playerGuessBoard);				//calls create board function to create the player's board
+		setup.drawBoard(playerGuessBoard);					//draws the player's guessing board
 		
 		System.out.println();
-		//System.out.println("Board for displaying cpu's guesses");
-		String [][] cpuGuessBoard = new String[11][11];	
-		setup.createBoard(cpuGuessBoard);
-		//setup.drawBoard(cpuGuessBoard);												//Draws the cpu's guessing board. Commented out for regular play
+		//System.out.println("Cpu's boats");				//for testing. Not used for normal play
+		String [][] cpuBoard = new String[11][11];			//creates a new double array for the cpu's board
+		setup.createBoard(cpuBoard);						//Calls create board function to create the cpu's board
+		setup.cpuSetup(cpuBoard);							//Calls the cput setup function to randomize cpu's boats
+		//setup.drawBoard(cpuBoard);						//for testing. Draws the cpu's board. Commented out for regular play
+		
+		System.out.println();
+		//System.out.println("Cpu's guesses");				//for testing. Not used for normal play
+		String [][] cpuGuessBoard = new String[11][11];		//creates a new double array for the cpu's guesses
+		setup.createBoard(cpuGuessBoard);					//Calls create board function to create cpu's gueesing board
+		//setup.drawBoard(cpuGuessBoard);					//for testing. Draws the cpu's guessing board. Commented out for regular play
+		
 		
 		//********Sart of Game!*************
-		
-		
 		while (playerScore<17 && cpuScore<17){											//Starts the game. While neither player has the score limit, do the following
 			//****Player's guess*****
 			turn++;
@@ -82,22 +78,22 @@ public class Battleship {
 			int cr= cpuGuess[0];														//set first value returned from function as row of guess
 			int cc= cpuGuess[1];														//set second value returned from function as col of guess
 			
-			while (cpuGuessBoard[r][c]==" X " || cpuGuessBoard[r][c]==" O "){		//checks if player already guess this spot
+			while (cpuGuessBoard[cr][cc]==" X " || cpuGuessBoard[cr][cc]==" O "){		//checks if player already guess this spot. randomizes new guess if needed
+				System.out.println("1");
 				setup.cpuGuess(cpuGuess);
-				cr= playerGuess[0];
-				cc= playerGuess[1];
+				cr= cpuGuess[0];
+				cc= cpuGuess[1];
 				
 			}
 			System.out.println("The Computer guessed "+cr+","+cc);
-			
-			if(playerBoard[r][c]==" - "){													//check if player's guess missed. Player does not score a point.
-				cpuGuessBoard[r][c]=" O ";											//marks miss as a O
+			if(playerBoard[cr][cc]==" - "){												//check if player's guess missed. Player does not score a point.
+				cpuGuessBoard[cr][cc]=" O ";											//marks miss as a O
 				//setup.drawBoard(cpuGuessBoard);										//draws the new guess board
 				System.out.println("The Computer Missed!");
 				System.out.println();
 			}
 			else{																		//if other conditions aren't met, it means player hit a cpu's boat
-				cpuGuessBoard[r][c]=" X ";												//marks hit as an X									
+				cpuGuessBoard[cr][cc]=" X ";											//marks hit as an X									
 				cpuScore++;																//players score increases by one
 				//setup.drawBoard(cpuGuessBoard);										//draws the new guess board
 				System.out.println("Hit!");
@@ -105,36 +101,22 @@ public class Battleship {
 				System.out.println("Cpu Score: "+cpuScore);								//displays cpu's new score. 
 				System.out.println();
 			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		}
-		System.out.println();
+		}	
+		
+		//*******End of Game**********
 		System.out.println();
 		System.out.println();
 		
-		if(playerScore>cpuScore){
+		if(playerScore>cpuScore){													//if player wins
 			System.out.println("You Sunk My Battleship!");
 			System.out.println("***Congradulations!***");
 			System.out.println("***Winner!***");
 			System.out.println("It took "+turn+"'s for you to win");
 		}
-		else{
-			System.out.println("The Computer won?");
+		else{																		//else, CPU wins
+			System.out.println("The Computer won?");	
 			System.out.println("***Loser!***");
-			System.out.println("It took "+turn+"'s for the cpu to win");
+			System.out.println("It took "+turn+" turn's for the cpu to win");
 		}
-		
-		
-		
-		
-		
 	}
-	
 }
--- setup.java
+++ setup.java
@@ -1,90 +1,123 @@
 import java.util.Scanner;
 
 public class setup{
+	
 	//************FUNCTION 1**************
-	public static void createBoard(String[][] board){								//This funtion sets up a board for the human player			
-		for(int r = 0; r<board.length; r++){										//set up all spaces as empty
-			for(int c = 0; c<board[0].length; c++){
-				board[r][c] = " - ";
+	public static void createBoard(String[][] board){					//This function sets up each board. Takes a double array as input from 			
+		for(int r = 0; r<board.length; r++){							//main function and modifies it to have standard board setup
+			for(int c = 0; c<board[0].length; c++){						//enters the first row and column to have numbers and letters.
+				board[r][c] = " - ";									//1.) set up each "space" as blank "-".
 			}
 		}
-		for(int r=1;r<board.length; r++){											//set up the numbers across the top
+		for(int r=1;r<board.length; r++){								//2.)Sets the top row to numbers 1-10 "|N|"
 			board[0][r]="|"+Integer.toString(r)+"|";
 		}
-		String[] columns = {"A","B","C","D","E","F","G","H","I","J"};				//set up the string of letters for rows
-		for(int c=1;c<board.length; c++){											//set up the letters going down
+		String[] columns = {"A","B","C","D","E","F","G","H","I","J"};	//3.)Set up the string of letters for rows
+		for(int c=1;c<board.length; c++){								//   Sets the first column to these letters "|A|"
 			board[c][0]="|"+columns[c-1]+"|";
 		}
-			board[0][0]="   ";														//formats top left square to blank. 
+			board[0][0]="   ";											//4.)Formats the top "Space" to blank. 
 	}	
 	
 	
-	
 	//************FUNCTION 2**************
-	public static String[][] setPiece(String[][] playerBoard){								//This function sets up a boat for the human player
+	public static String[][] setPiece(String[][] playerBoard){			//This function allows the player to set each boat. Takes the players board					
+																		//from the main function as input and will return the new board. 
+		int [] boats ={5,4,3,3,2};										//This function uses scanner util to ask player questions and take response
+		for (int j=0; j<5; j++){										//as an input. There is a checking part to check if requested placement is ok
+			int L = boats[j];											//then will set the "spaces" in on player's board to the length of the boat
+			int C =0;
+			int R =0;													//Variables are initiated to prevent errors in while loop.
+			int check =0;												//R=Row,C=col, L=length, P=position(V OR H), check if for placement check.
+			int P = 0;													
 			
-		int [] boats ={5,4,3,3,2};															//set up the boat lengths						
-		for (int j=0; j<5; j++){															//for loop to set up each boat
-			int L = boats[j];																//new int to pull length
-			System.out.println("Where would you like to place this boat("+L+" Length)");
-			Scanner in = new Scanner(System.in);											//Scanner function to read in inputs
-			System.out.print("Column of ship (1-10):");										//Ask player for which column boat will start in
-			String input1 = in.next();														//Next input is brought in as a string
-			int C = Integer.parseInt(input1);												//set string input to a int variable C
-			
-			System.out.print("Row of ship (1-10):");										//Ask player for which row boat will start in
-			String input2 = in.next();														//Next input is brought in as a string
-			int R = Integer.parseInt(input2);												//set string input to a int variable R
-			
-			System.out.print("Vertical or Horizontal? (V=1 Or H=2):");						//Ask player if they want the boat vertical or Horizontal
-			String input4 = in.next();														//Next input is brought in as a string
-			int P = Integer.parseInt(input4);												//set string input to a int Variable P
-			
-			if (P==2){
-				while (C<1||C+L>11){															//Checks if that is whithin boundries. Aks player to guess again if outside.
-					System.out.println("This is not a valid Column. Pick another");
-					System.out.print("Column(1-10):");										
-					input1 = in.next();														
-					C = Integer.parseInt(input1);
+			while(check!=L){											//while loop to check if placement is allowed. Reset check to 0 after each attempt.
+				check=0;												//Check is increased for each sucessful test of placement of boat. 
+				Scanner in = new Scanner(System.in);					//Each time it checks, it will ask player the following questions and set C,R,and P
+				System.out.println("Where would you like to place this boat("+L+" Length)");
+				System.out.print("Column of ship (1-10):");										
+				String input1 = in.next();														
+				C = Integer.parseInt(input1);
+				
+				System.out.print("Row of ship (1-10):");				
+				String input2 = in.next();														
+				R = Integer.parseInt(input2);
+				
+				System.out.print("Vertical or Horizontal? (V=1 Or H=2):");						
+				String input4 = in.next();														
+				P = Integer.parseInt(input4);
+				
+				if(C+L<12 || C>0 || R+L <12 || R<0){							//Checks horizontal placement of boats by checking each "space" and
+					if(P==2){													//increasing check if it works. If Check is same as length at end of 
+						for(int i=C;i<L+C;i++){									//loop, then the placement passes test. 
+							if(playerBoard[R][i]==" - "){
+							check++;}
+						}
+					}
+					else{
+						for(int i=R;i<L+R;i++){									//Checks verticle placement of boats.
+							if(playerBoard[i][C]==" - "){
+							check++;}
+						}
+					}
+				}
+				
+				if(check!=L){													//if check fails, it will tell the playe, and run while loop again. 
+					System.out.println("This is not a valid placement for this boat. Please try again.");
 				}
 			}
-			else{
-				while (R<1||R+L>11){																//Checks if that is whithin boundries. Aks player to guess again if outside.
-					System.out.println("This is not a valid Row. Pick another");
-					System.out.print("Row(1-10):");										
-					input2 = in.next();														
-					R = Integer.parseInt(input2);
-				}
-			}
-
+			
 			System.out.println();
-			if(P==2){
-				//if P==2, then print L across the designated spaces horizontally
+			if(P==2){																//if P==2, then print L across the designated spaces horizontally
 				for (int i=C;i<L+C;i++){
-					playerBoard[R][i]=" "+L+" ";
+					playerBoard[R][i]=" "+L+" ";									//sets each space to the N of the boat length.
 				}
 			}
-			else{																			//else, then print L across the designated spaces vertiacally
+			else{																	//else, then print L across the designated spaces vertiacally
 				for (int i=R;i<L+R;i++){
 					playerBoard[i][C]=" "+L+" ";
 				}
 			}
-			drawBoard(playerBoard);															//draws the board using the draw board function. will run after each placement
+			drawBoard(playerBoard);													//draws the board using the draw board function. will run after each placement
 		}
-		return playerBoard;																	//returns the array to the main function
+		return playerBoard;															//returns the array to the main function
 	}
+	
+	
 		
 	//************FUNCTION 3**************
-	public static String[][] cpuSetup(String[][] cpuBoard){			 
-			
-		int [] boats ={5,4,3,3,2};										//set the length of each boat
-
+	public static String[][] cpuSetup(String[][] cpuBoard){				//This function will set up the cpu's board. The cpu will randomly generate a  
+																		//staring "space" and a direction (V or H). Then will check if placement is ok.
+		int [] boats ={5,4,3,3,2};										//If placement not ok, then will try again. 
+		int check=0;													//This function requires the cpu board as input from main. Modifies and returns to main. 
+		int L=1;														//Variables initiated for function. Same as used for player setup
+		int C=0;														//L is first set to 1 to avoid conflict in while loop below.
+		int R=0;
+		double P=0;
+		
 		for (int j=0; j<5; j++){										//for each boat do the following
-			int L = boats[j];											//set length
-			int C = (int)(Math.random()*(10-L))+1;						//randomly set a C (within bounds of array)
-			int R = (int)(Math.random()*(10-L))+1;						//randomly set a R (within bounds of array)
-			double P =Math.random();									//randomly set P. P>.5 is horizontally. Else vertiacally
-			
+			check=0;													//reset check variable
+			while(check!=L){											//while loop for checking if placement if ok
+				check=0;												//resets check again. There seemed be error if not done
+				L = boats[j];											// Sets length of boat to the next length given in string array above
+				C = (int)(Math.random()*(11-L))+1;						//randomly selects a starting column. (11-L) will prevent bad starting spot
+				R = (int)(Math.random()*(11-L))+1;						//randomly selects a starting row
+				P =Math.random();										//randomly create a double variable P. For V or H placement
+	
+				if(P>.5){												//if horizontally, check all horizontal spaces if ok.
+					for(int i=C;i<L+C;i++){								//as before, checks if placement ok for each "space" and 
+						if(cpuBoard[R][i]==" - "){						//increases check by 1 for each ok space. If all spaces ok, 
+						check++;}										//then check passes
+					}
+				}
+				else{
+					for(int i=R;i<L+R;i++){
+						if(cpuBoard[i][C]==" - "){
+							check++;}
+					}
+				}
+			}
+				
 			if(P>.5){
 				for (int i=C;i<L+C;i++){								//print L acorss the designated spaces horizontally
 					cpuBoard[R][i]=" "+L+" ";
@@ -102,18 +135,18 @@ public class setup{
 		
 		
 	//************FUNCTION 4**************
-	public static void drawBoard (String[][] Board){				//Function 4: DrawBaord -Prints out the board for the player
-			for (int i=0; i < Board.length;i++) {
+	public static void drawBoard (String[][] Board){			//Function 4: DrawBaord -Prints out the board for the player
+			for (int i=0; i < Board.length;i++) {				//Takes any board from main. and prints it for the player.
 			for(int j=0; j < Board.length;j++) {
-				System.out.printf(Board[i][j]);						//simple double array printing
+				System.out.printf(Board[i][j]);					//simple double array printing
 			}
 			System.out.println();
 			}
 	}
 
 	//***********FUNCTION 5*************
-	public static int[] playerGuess (int[] playerGuess){		//Function 5: Input the player's guess
-	
+	public static int[] playerGuess (int[] playerGuess){		//Function 5: Input the player's guess. The function uses scanner util to take
+																//in player's answers. Then checks if it's a logical guess. Returns to guess to main
 			System.out.println("Please make a Guess");			//Asks player for a column. Parses it as an int and sets it to variable C
 			Scanner in = new Scanner(System.in);											
 			System.out.print("Column(1-10):");										
@@ -143,8 +176,8 @@ public class setup{
 	
 	
 	//***********FUNCTION 6*************
-	public static int[] cpuGuess (int[] cpuGuess){		//Function 6: Randomized guess for CPS
-															
+	public static int[] cpuGuess (int[] cpuGuess){		//Function 6: Randomized guess for CPU. Takes an int string from main. Returns
+														//a randomized guess back to main. Will check if guess is logical.
 			int C = (int)(Math.random()*11);			//random Row and column are selected. 
 			while (C<1||C>11){							//if outside boundries it will randomize a new number																
 				C = (int)(Math.random()*11);
@@ -157,7 +190,7 @@ public class setup{
 			cpuGuess[0]=R;								//sets the array to the cpu's guess. Passes it back to main function
 			cpuGuess[1]=C;
 	
-		return cpuGuess;	
+		return cpuGuess;								
 	}
 
 
